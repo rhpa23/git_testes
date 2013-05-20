1b4d478c5a6e658b90d59b256fa7f3a13335cd76
Rudolf Aragao <rudolf.aragao@hp.com>
2013-05-20 11:01:38 -0300

    new commit message

--- Cliente.java
+++ Cliente.java
@@ -1,24 +1,5 @@
 public class Cliente	{
 	public Cliente()	{
-		Object one = 2;
-		Object one = 3;
-		Object one = 4;
-		Object one = 5;
-		Object one = 6;
-		Object one = 2;
-		Object one = 2;
-		Object one = 2;
-		Object one = 2;
-		Object one = 2;
-		Object one = 2;
-		Object one = 2;
-		Object one = 2;
-		Object one = 2;
-		Object one = 2;
-		Object one = 2;
-		Object one = 2;
-		Object one = 2;
-		Object one = 2;
-		
+		Object one = 1;
 	}
 }
